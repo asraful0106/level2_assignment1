95f9817e7ecf85b2d1321503ddcc62deaf4c8be9
asraful0106 <asrafulapf@gmail.com>
2025-05-08 09:19:47 +0600

    Q-5

--- src/solution.ts
+++ src/solution.ts
@@ -50,3 +50,13 @@ class Car extends Vehicle{
 
 
 
+function processValue(value: string | number): number{
+    if(typeof value == 'string'){
+        return value.length;
+    }else{
+        return value*2;
+    }
+}
+
+
+
